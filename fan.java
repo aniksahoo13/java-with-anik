5e0f1cdf74afc1f62712a87e3c79a657d40df81d
Anik Sahoo <sahooanik50@gmail.com>
2026-05-30 14:10:32 +0530

    Add files via upload

---
+++ fan.java
@@ -0,0 +1,9 @@
+public class fan {
+    public static void main(String[] args) {
+        System.out.println("my fabaret game football");
+        System.out.println("I love my famale ");
+        System.out.println("and 'I' love my mother ");
+        System.out.println("anit 'I' live my brother and fother");
+        
+    }
+}
