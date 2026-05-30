9bb38342deba11334b1cff7b917b55ad21a51db8
Anik Sahoo <sahooanik50@gmail.com>
2026-05-30 13:57:52 +0530

    Add files via upload

---
+++ load.java
@@ -0,0 +1,7 @@
+public class load {
+    public static void main(String[] args) {
+        System.out.println("my fabaret load name is shiv");
+        System.out.println("my fabaret subject is math");
+        
+    }
+}
