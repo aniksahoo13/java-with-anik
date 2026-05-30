53e934453a70b8a805aa2341bd3da560720d3542
aniksahoo13 <sahooanik50@gmail.com>
2026-05-30 11:00:05 +0530

    Add files via upload

---
+++ box.java
@@ -0,0 +1,6 @@
+public class box {
+    public static void main (String[] args) {
+        System.out.println("hello java");
+        System.out.println("fdgsdgsdgsg");
+    }
+}
---
+++ swallow.java
@@ -0,0 +1,7 @@
+public class swallow {
+    public static void main(String[] args) {
+        System.out.println("hellow anik");
+        System.out.println("i love pizzaaaaaaaaaaaaaaaaaa");
+        
+    }
+}
