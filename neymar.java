6f35516480c77ee42d9878693c7478ee881f3d38
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 17:49:14 +0530

    Add files via upload

---
+++ neymar.java
@@ -0,0 +1,7 @@
+public class neymar {
+    public static void main(String[] args) {
+        System.out.println("the name of my fabarit player name is neymar");
+        System.out.println("neymar lived in a beautiful playes is brizil");
+        
+    }
+}
