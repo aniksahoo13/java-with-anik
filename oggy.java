b951a7fd071a225a99d31ef0aa27a2314cc6e811
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 12:47:10 +0530

    Add files via upload

---
+++ oggy.java
@@ -0,0 +1,6 @@
+public class oggy {
+    public static void main(String[] args){
+        System.out.println("I like to watch movies");
+        System.out.println("like Theri, Begil etc");
+    }
+}
