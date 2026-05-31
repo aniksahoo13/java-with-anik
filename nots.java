85e447152c3d154f26069bc8a18ed7a925ea830d
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 17:54:18 +0530

    Add files via upload

---
+++ nots.java
@@ -0,0 +1,7 @@
+public class nots {
+    public static void main(String[] args) {
+        System.out.println("the name of my laptop is HP");
+        System.out.println("the beautiful bird name is parot");
+        
+    }
+}
