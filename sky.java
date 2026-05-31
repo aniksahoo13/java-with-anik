abb6f79b93afec3f3041960e3e8c1a22b8f57b82
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 15:29:46 +0530

    Add files via upload

---
+++ sky.java
@@ -0,0 +1,7 @@
+public class sky {
+    public static void main(String[] args) {
+        System.out.println("their are somane people in frount of our house");
+        System.out.println("their somane mobail in frount of our house");
+        
+    }
+}
