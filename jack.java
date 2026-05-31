93f707af03020b07fa0e168b0bd66b0bb0a7e7d3
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 21:46:22 +0530

    Add files via upload

---
+++ jack.java
@@ -0,0 +1,7 @@
+public class jack {
+    public static void main(String[] args) {
+        System.out.println("A bird had been caught by him");
+        System.out.println("A [oem had been done by him");
+        
+    }
+}
