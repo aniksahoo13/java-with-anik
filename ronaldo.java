550306d9d5a4b5ea0988a39086144e486e677159
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 17:44:50 +0530

    Add files via upload

---
+++ ronaldo.java
@@ -0,0 +1,7 @@
+public class ronaldo {
+    public static void main(String[] args) {
+        System.out.println("the name of the mobail is oppo");
+        System.out.println("the name of my fabarit car is lamborgine");
+        
+    }
+}
