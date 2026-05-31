1b480fefe038cab4494b16559c377c0fff99934a
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 13:13:07 +0530

    Add files via upload

---
+++ light.java
@@ -0,0 +1,7 @@
+public class light {
+    public static void main(String[] args) {
+        System.out.println("our school name is paskura B.B. Hish school");
+        System.out.println("our school is a beautiful school");
+        
+    }
+}
