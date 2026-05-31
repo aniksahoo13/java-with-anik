79ea705f276d789bc38d196f9a03ba2371b0b366
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 12:59:18 +0530

    Add files via upload

---
+++ CID.java
@@ -0,0 +1,7 @@
+public class CID {
+    public static void main(String[] args) {
+        System.out.println("I like to watch catus");
+        System.out.println("and I also like to watch football match");
+
+    }
+}
---
+++ boy.java
@@ -0,0 +1,7 @@
+public class boy {
+    public static void main(String[] args) {
+        System.out.println("I like my football player is Ronaldo");
+        System.out.println("and 2nd football palyer's name Nemar, Mbappa");
+        
+    }
+}
