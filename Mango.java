9b1e373ac6b8f8d0e78b5c2bee34624c77e5eda1
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 13:06:55 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,7 @@
+public class Mango {
+    public static void main(String[] args) {
+        System.out.println("I like to eat fruits");
+        System.out.println("and my favaright fruit is Mango");
+        
+    }
+}
