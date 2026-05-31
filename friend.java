ee133db97a867accab76e2413a169f28b80b87e2
Anik Sahoo <sahooanik50@gmail.com>
2026-05-31 15:39:31 +0530

    Add files via upload

---
+++ friend.java
@@ -0,0 +1,7 @@
+public class friend {
+    public static void main(String[] args) {
+        System.out.println("my best friend name is anikan");
+        System.out.println("and my 2nd best friend name is rohit");
+        
+    }
+}
