1d67ad73384d50f921034510f5bf753fefb0fd02
Anik Sahoo <sahooanik50@gmail.com>
2026-06-01 22:25:00 +0530

    Add files via upload

---
+++ School.java
@@ -0,0 +1,7 @@
+public class School {
+    public static void main(String[] args) {
+        System.out.println("I see you have a golden heart");
+        System.out.println("Swallow, little Swallow, plase stay with me for one night");
+
+    }
+}
