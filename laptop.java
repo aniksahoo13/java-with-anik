648d2af6659b0e0dac50e30433fea9b59b18e1e8
Anik Sahoo <sahooanik50@gmail.com>
2026-06-02 20:55:05 +0530

    Add files via upload

---
+++ laptop.java
@@ -0,0 +1,7 @@
+public class laptop {
+    public static void main(String[] args) {
+        System.out.println("I like to do java");
+        System.out.println("The name of our school is Pas kura B.B. hIgh school");
+        
+    }
+}
